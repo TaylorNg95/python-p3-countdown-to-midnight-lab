9958968efaf1f38d615edb42a3679038b29c55e4
Taylor Ng <taylormng95@gmail.com>
2024-06-24 10:12:27 -0400

    completes midnight lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
 # your code goes here!
+
+import time
+
+def countdown(int):
+    start = int
+    while(start > 0):
+        print(f'{start} SECOND(S)!')
+        start -= 1
+    print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(int):
+    start = int
+    while(start > 0):
+        print(f'{start} SECOND(S)!')
+        start -= 1
+        time.sleep(1)
+    print('HAPPY NEW YEAR!')
+countdown_with_sleep(5)
